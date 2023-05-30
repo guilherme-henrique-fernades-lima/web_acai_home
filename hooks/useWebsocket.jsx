12686392116925f77a6721664e6385218476faa0
Matheus-Dev <darkaixxt@gmail.com>
2023-05-30 20:39:09 -0300

    Cria novo diretorio para hooks e adiciona modulo de websockets

---
+++ hooks/useWebsocket.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const WebSocket = () => {
+    const [evento, setEvento] = useState({});
+
+    let socket;
+
+    useEffect(() => {
+
+        // Start WebSocket
+        const socketInitializer = async () => {
+            await fetch("/api/websocket");
+            socket = io();
+
+            console.log("SOCKET>>>", socket)
+
+            socket.on("connect", () => {
+                console.log("Socket Conectado!");
+            });
+
+            socket.on("EVENT", (value) => {
+                console.log("recebendo new event...", value);
+                setEvento({ 'EVENT': value })
+            });
+
+        };
+
+        // End WebSocket
+        socketInitializer();
+
+        return () => {
+            console.log("Socket Desconectado!");
+            socket.close();
+        };
+
+    }, [])
+
+    return {
+        evento,
+    };
+};
+
+export default WebSocket;
