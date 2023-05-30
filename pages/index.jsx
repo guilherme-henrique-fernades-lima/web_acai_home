2638b23ea7a9aef74e6510532b867cce6dccc301
Matheus-Dev <darkaixxt@gmail.com>
2023-05-30 20:40:10 -0300

    Adiciona conexao websockets na pagina index.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import useWebSocket from "@/hooks/useWebSocket";
+
 import { Button } from "@mui/material";
 
+
 export default function Home() {
+   const { evento } = useWebSocket();
+
+    useEffect(() => {
+        if (evento.NOVO_PEDIDO) {
+            //Logica de negocio
+            console.log("EVENTO>>>", evento)
+
+        }
+    }, [evento]);
+
   return (
     <Button variant="contained" disableElevation>
       Página home da aplicação.
