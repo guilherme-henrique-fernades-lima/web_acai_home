3bb125ea4beab0262ddb7807adf8b93c6c79dd15
Matheus-Dev <darkaixxt@gmail.com>
2023-06-03 18:09:34 -0300

    Adiciona no app novo context API de Auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,19 +2,21 @@
 import Layout from "../components/Layout";
 
 //Contexto para temas e dados para mandar entre as páginas
-import ThemeContext from "../context/ThemeContext";
+import ThemeContext from "@/context/ThemeContext";
+import AuthContext from "@/context/AuthContext";
 import GlobalStyles from "../styles/GlobalStyles";
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <AuthContext>
       <GlobalStyles />
       <ThemeContext>
         <Layout>
             <Component {...pageProps} />
         </Layout>
       </ThemeContext>
-    </>
+    </AuthContext>
   );
 }
 
