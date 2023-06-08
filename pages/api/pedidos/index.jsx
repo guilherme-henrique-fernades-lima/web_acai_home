e84402f32c69d4b34dfb390b2728872f0a2ec6ee
Matheus-Dev <darkaixxt@gmail.com>
2023-06-07 22:01:44 -0300

    Cria novo endpoint para pedidos

---
+++ pages/api/pedidos/index.jsx
@@ -0,0 +1,31 @@
+async function getPedidos(req, res) {
+
+    const token = req.headers.authorization
+
+    const result = await fetch(`${process.env.NEXT_URL_BACKEND}/pedidos`, {
+        method: 'GET',
+        headers: {
+            "X-Requested-With": "XMLHttpRequest",
+            "Content-Type": "application/json;charset=UTF-8",
+            Authorization: `Bearer ${token}`
+        }
+    });
+
+    const data = await result.json();
+
+    return res.status(result.status).json(data);
+
+}
+
+
+export default async function handler(req, res) {
+
+    if (req.method == 'GET') {
+        getPedidos(req, res);
+
+    } else {
+        res.status(405).send()
+
+    }
+
+}
