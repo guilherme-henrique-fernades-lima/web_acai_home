d596ad3ac3061d21c3502f3cc44522511d4aefdb
Matheus-Dev <darkaixxt@gmail.com>
2023-06-24 14:55:29 -0300

    Adiciona console de depuracao

--- pages/api/auth/sign-in.jsx
+++ pages/api/auth/sign-in.jsx
@@ -2,6 +2,8 @@ async function signInRequest(req, res) {
 
     const payload = req.body
 
+    console.log("SSR>>>", payload)
+
     const result = await fetch(`${process.env.NEXT_URL_BACKEND}/auth/login/`, {
         method: 'POST',
         headers: {
